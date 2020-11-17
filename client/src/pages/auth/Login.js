3f514e74e685d612c0f8080eba255cf781fe82cc
awais512 <raza.awais25@gmail.com>
2020-11-17 16:10:50 +0500

    Code Refactoring and Better Folder Structure

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import LoginForm from './LoginForm';
+import LoginForm from '../../components/Login/LoginForm';
 
 const Login = ({ history }) => {
   const [loading, setLoading] = useState(false);
--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import CompleteRegistrationForm from './CompleteRegistrationForm';
+import CompleteRegistrationForm from '../../components/Register/CompleteRegistrationForm';
 
-const RegisterComplete = () => {
+const RegisterComplete = ({ history }) => {
   return (
     <div className='container p-5'>
       <div className='row'>
         <div className='col-md-6 offset-md-3'>
           <h4>Register</h4>
-          <CompleteRegistrationForm />
+          <CompleteRegistrationForm history={history} />
         </div>
       </div>
     </div>
