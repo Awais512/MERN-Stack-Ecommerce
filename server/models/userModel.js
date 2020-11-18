3cc9cfe674c1134bc1170cb58b1d62f982e401da
awais512 <raza.awais25@gmail.com>
2020-11-18 11:20:47 +0500

    Added User Model and Created UserSchema

---
+++ server/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    role: {
+      type: String,
+      default: 'subscriber',
+    },
+
+    cart: {
+      type: Array,
+      default: [],
+    },
+
+    address: String,
+    // whishlist :[ {type: ObjectId, ref:'Product'} ]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
