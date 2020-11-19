99179f86582fc5084be61eb772bb7c18a18930ad
awais512 <raza.awais25@gmail.com>
2020-11-19 10:25:46 +0500

    Side Nav for users

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import UserNav from '../../components/nav/UserNav';
 
 const History = () => {
   return (
     <div className='container-fluid'>
       <div className='row'>
-        <div className='col'>User Page</div>
+        <div className='col-md-2'>
+          <UserNav />
+        </div>
+        <div className='col'>User History Page</div>
       </div>
     </div>
   );
