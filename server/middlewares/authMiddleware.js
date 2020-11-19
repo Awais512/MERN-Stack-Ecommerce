87b8667f30021ccbcca10e4a0a5f534172232d05
awais512 <raza.awais25@gmail.com>
2020-11-19 12:58:23 +0500

    Admin Check Middleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase');
+const User = require('../models/userModel');
 
 exports.authCheck = async (req, res, next) => {
   try {
@@ -12,5 +13,16 @@ exports.authCheck = async (req, res, next) => {
     res.status(401).json({
       err: 'Invalid or expired token',
     });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+
+  const adminUser = await (await User.findOne({ email })).exec();
+  if (adminUser.role !== 'admin') {
+    res.status(403).json({ err: 'You are not uthorized to view this page' });
+  } else {
+    next();
   }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,11 @@ const {
   createOrUpdateUser,
   currentUser,
 } = require('../controllers/authController');
-const { authCheck } = require('../middlewares/authMiddleware');
+const { authCheck, adminCheck } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
