99ff70058cbd30d513c3f72221e057d9fb0ef2ee
awais512 <raza.awais25@gmail.com>
2020-11-21 16:19:43 +0500

    Categories crud requests and Dynamic admin dashboard

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -56,8 +56,18 @@ const Header = () => {
           title={user.email && user.email.split('@')[0]}
           className='float-right'
         >
-          <Item key='setting:1'>Option 1</Item>
-          <Item key='setting:2'>Option 2</Item>
+          {user && user.role === 'subscriber' && (
+            <Item key='setting:1'>
+              <Link to='/user/history'>Dashboard</Link>
+            </Item>
+          )}
+
+          {user && user.role === 'admin' && (
+            <Item key='setting:1'>
+              <Link to='/admin/dashboard'>Dashboard</Link>
+            </Item>
+          )}
+
           <Item icon={<LogoutOutlined />} onClick={logout}>
             Logout
           </Item>
---
+++ client/src/functions/categories.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+//Get All Categories
+export const getCategories = async () =>
+  await axios.get(`${process.env.REACT_APP_API}/category`);
+
+//Get Category By Id
+export const getCategory = async (slug) =>
+  await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
+
+//Remove Categories
+export const removeCategory = async (slug, authtoken) => {
+  const config = {
+    headers: {
+      authtoken,
+    },
+  };
+  return await axios.delete(
+    `${process.env.REACT_APP_API}/category/${slug}`,
+    config
+  );
+};
+
+//Update Categories
+export const updateCategory = async (slug, category, authtoken) => {
+  const config = {
+    headers: {
+      authtoken,
+    },
+  };
+  return await axios.put(
+    `${process.env.REACT_APP_API}/category/${slug}`,
+    category,
+    config
+  );
+};
+
+//Create Categories
+export const createCategory = async (category, authtoken) => {
+  const config = {
+    headers: {
+      authtoken,
+    },
+  };
+  return await axios.post(
+    `${process.env.REACT_APP_API}/category`,
+    category,
+    config
+  );
+};
