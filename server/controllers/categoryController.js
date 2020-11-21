e04fb1a31e4e3440d12b4da69b9f7788db30bb19
awais512 <raza.awais25@gmail.com>
2020-11-21 15:04:45 +0500

    Create and List Categories

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,18 +1,45 @@
 const Category = require('../models/categoryModel');
 const asyncHandler = require('express-async-handler');
+const slugify = require('slugify');
 
 //@desc     Create New Category
 //@Route    POST /api/category
 //@access   Private/Admin
-exports.createCategory = asyncHandler(async (req, res) => {});
+exports.createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await new Category({ name, slug: slugify(name) }).save();
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
 //@desc     GET all Categories
 //@Route    GET /api/category
 //@access   Private/Admin
-exports.getCategories = asyncHandler(async (req, res) => {});
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(400).json({ err: err.message });
+  }
+};
 //@desc     Get single Category
 //@Route    GET /api/category/:id
 //@access   Private/Admin
-exports.getCategory = asyncHandler(async (req, res) => {});
+exports.getCategory = asyncHandler(async (req, res) => {
+  //   try {
+  //     const category = await Category.findById(req.params.slug);
+  //     if (category) {
+  //       res.status(200).json(category);
+  //     } else {
+  //       return res.status(404).json({ msg: 'Category does not Exist' });
+  //     }
+  //   } catch (err) {
+  //     res.status(400).json({ err: err.message });
+  //   }
+});
 //@desc     Update Category
 //@Route    PUT /api/category/:id
 //@access   Private/Admin
