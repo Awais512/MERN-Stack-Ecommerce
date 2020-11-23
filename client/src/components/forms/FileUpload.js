9410321a2b7519d79e8ad6da27026c18bb0ef4d2
awais512 <raza.awais25@gmail.com>
2020-11-23 21:14:11 +0500

    Image Resizing

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import axios from 'axios';
+import Resizer from 'react-image-file-resizer';
+import { useSelector } from 'react-redux';
 
 const FileUpload = () => {
+  const { user } = useSelector((state) => ({ ...state }));
   const fileUploadAndResize = (e) => {
     // console.log(e.target.files);
+    let files = e.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          'JPEG',
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          'base64'
+        );
+      }
+    }
   };
 
   return (
