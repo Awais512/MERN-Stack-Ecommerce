3efbf0e714f6342dbc4990476d404ed74423206b
awais512 <raza.awais25@gmail.com>
2020-11-24 12:25:38 +0500

    Admin products card component

---
+++ client/src/components/Cards/AdminProductsCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card } from 'antd';
+const { Meta } = Card;
+
+const AdminProductsCard = ({ product }) => {
+  const { title, description, images } = product;
+  return (
+    <Card
+      hoverable
+      style={{ width: '240' }}
+      cover={
+        <img
+          src={images && images.length ? images[0].url : ''}
+          className='m-2'
+          style={{ height: '150px', objectFit: 'cover' }}
+          alt={title}
+        />
+      }
+    >
+      <Meta title={title} description={description} />
+    </Card>
+  );
+};
+
+export default AdminProductsCard;
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import AdminProductsCard from '../../components/Cards/AdminProductsCard';
 import AdminNav from '../../components/nav/AdminNav';
 
 import { getProductsByCount } from '../../functions/products';
 
-const Dashboard = () => {
-  const [products, setProducts] = useState('');
+const AdminDashboard = () => {
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const getProductsCount = async () => {
-      setLoading(true);
-      try {
-        const res = await getProductsByCount(100);
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => {
         setProducts(res.data);
         setLoading(false);
-        console.log(res.data);
-      } catch (error) {
-        console.log(error);
+      })
+      .catch((err) => {
         setLoading(false);
-      }
-    };
-
-    getProductsCount();
-  }, []);
+        console.log(err);
+      });
+  };
 
   return (
     <div className='container-fluid'>
@@ -30,15 +31,23 @@ const Dashboard = () => {
         <div className='col-md-2'>
           <AdminNav />
         </div>
-        {loading ? (
-          <h4 className='text-danger'>Loading...</h4>
-        ) : (
-          <h4>All Products</h4>
-        )}
-        <div className='col'>{JSON.stringify(products)}</div>
+
+        <div className='col'>
+          {loading ? (
+            <h4 className='text-danger'>Loading...</h4>
+          ) : (
+            <h4>All Products</h4>
+          )}
+          <div className='row'>
+            {products.map((product) => (
+              <div key={product._id} className='col-md-4'>
+                <AdminProductsCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
-
-export default Dashboard;
+export default AdminDashboard;
