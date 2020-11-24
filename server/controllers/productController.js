f9a3403a83f6aec300e38ea78f3fe7ac6b02a6b5
awais512 <raza.awais25@gmail.com>
2020-11-24 17:14:59 +0500

    Delete Product EndPoint

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -20,16 +20,25 @@ exports.createProduct = async (req, res) => {
 //@access   Private/admin
 exports.getProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
-    res.json(products);
-  } catch (err) {
-    console.log(err);
-    res
-      .status(400)
-      .json({ err: err.message })
+    const products = await Product.find({})
       .limit(parseInt(req.params.count))
       .populate('category')
       .populate('subs')
       .sort([['createdAt', 'desc']]);
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//@desc     Delete Product
+//@Route    DELETE /api/product/:slug
+//@access   Private/admin
+exports.deleteProducts = async (req, res) => {
+  try {
+    const product = await Product.findOneAndRemove({ slug: req.params.slug });
+    res.status(204).json({ msg: 'Deleted Successfully' });
+  } catch (error) {
+    res.status(400).send('Delete failed');
   }
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -6,9 +6,11 @@ const { authCheck, adminCheck } = require('../middlewares/authMiddleware');
 const {
   createProduct,
   getProducts,
+  deleteProducts,
 } = require('../controllers/productController');
 
 router.post('/product', authCheck, adminCheck, createProduct);
 router.get('/product/:count', getProducts);
+router.delete('/product/:slug', authCheck, adminCheck, deleteProducts);
 
 module.exports = router;
