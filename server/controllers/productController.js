43cc6ed6795cb2adf6ef88530b2d402ff75972b5
awais512 <raza.awais25@gmail.com>
2020-11-26 20:27:03 +0500

    List Endpoint with pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -66,15 +66,37 @@ exports.updateProduct = async (req, res) => {
   }
 };
 
+//Without Pagination
+// exports.list = async (req, res) => {
+//   try {
+//     // createdAt/updatedAt, desc/asc, 3
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate('category')
+//       .populate('subs')
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+//With Pagination
 exports.list = async (req, res) => {
   try {
     // createdAt/updatedAt, desc/asc, 3
-    const { sort, order, limit } = req.body;
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3;
     const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
       .populate('category')
       .populate('subs')
       .sort([[sort, order]])
-      .limit(limit)
+      .limit(perPage)
       .exec();
 
     res.json(products);
