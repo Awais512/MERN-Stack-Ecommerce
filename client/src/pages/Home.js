49ba7ecdbbcda28ca990c71e59bd93761a8e4668
awais512 <raza.awais25@gmail.com>
2020-11-26 12:35:16 +0500

    Get Products in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getProductsByCount } from '../functions/products';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadAllProducts = async () => {
+    const { data } = await getProductsByCount(1);
+    setProducts(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
+      {JSON.stringify(products)}
     </div>
   );
 };
