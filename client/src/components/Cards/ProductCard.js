2b08e28ba239fe803821fec71ce2b8c6d68b1ec5
awais512 <raza.awais25@gmail.com>
2020-11-26 14:35:40 +0500

    Ant Design Card

--- client/src/components/Cards/ProductCard.js
+++ client/src/components/Cards/ProductCard.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Card } from 'antd';
+import { Link } from 'react-router-dom';
+import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import laptop from '../../images/laptop.png';
+const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+  const { title, description, slug, images } = product;
   return (
-    <div>
-      <h1>{product.title}</h1>
-    </div>
+    <Card
+      hoverable
+      style={{ width: '240' }}
+      cover={
+        <img
+          src={images && images.length ? images[0].url : laptop}
+          className='p-1'
+          style={{ height: '150px', objectFit: 'cover' }}
+          alt={title}
+        />
+      }
+      actions={[
+        <Link to={`/product/${slug}`}>
+          <EyeOutlined className='text-warning' /> <br /> View Product
+        </Link>,
+        <>
+          <ShoppingCartOutlined
+            className='text-danger'
+            //   onClick={() => handleRemove(slug)}
+          />
+          <br /> Add to Cart
+        </>,
+      ]}
+    >
+      <Meta
+        title={title}
+        description={`${description && description.substring(0, 40)}...`}
+      />
+    </Card>
   );
 };
 
