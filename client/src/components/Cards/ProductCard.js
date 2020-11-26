e25ffc56060836b285611efb45190bcfafa4aac7
awais512 <raza.awais25@gmail.com>
2020-11-26 14:24:36 +0500

    Product Card component

---
+++ client/src/components/Cards/ProductCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductCard = ({ product }) => {
+  return (
+    <div>
+      <h1>{product.title}</h1>
+    </div>
+  );
+};
+
+export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ProductCard from '../components/Cards/ProductCard';
 import { getProductsByCount } from '../functions/products';
 
 const Home = () => {
@@ -6,7 +7,9 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
 
   const loadAllProducts = async () => {
-    const { data } = await getProductsByCount(1);
+    setLoading(true);
+    const { data } = await getProductsByCount(6);
+    setLoading(false);
     setProducts(data);
     console.log(data);
   };
@@ -16,10 +19,20 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className='jumbotron'>
+        {loading ? <h1>Loading...</h1> : <h1>All Products</h1>}
+      </div>
+      <div className='container'>
+        <div className='row'>
+          {products.map((product) => (
+            <div key={product._id} className='col-md-4'>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
