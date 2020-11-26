79d471691fcaa36daa420c5831962a7845e7ca7f
awais512 <raza.awais25@gmail.com>
2020-11-26 23:03:48 +0500

    Image carousel

--- client/src/components/Cards/SingleProduct.js
+++ client/src/components/Cards/SingleProduct.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const { Meta } = Card;
 
 const SingleProduct = ({ product: { title, description, images, slug } }) => {
   return (
     <>
-      <div className='col-md-7'>image carousel</div>
+      <div className='col-md-7'>
+        <Carousel showArrows={true} autoPlay infiniteLoop>
+          {images &&
+            images.map((img) => (
+              <img src={img.url} key={img.public_id} alt={title} />
+            ))}
+        </Carousel>
+      </div>
 
       <div className='col-md-5'>
         <Card
