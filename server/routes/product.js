b7ed18b747f370a21f370af7ccdec4ab11c95139
awais512 <raza.awais25@gmail.com>
2020-11-26 20:17:52 +0500

    Get Products count endpoint

--- server/routes/product.js
+++ server/routes/product.js
@@ -12,11 +12,13 @@ const {
   remove,
   read,
   updateProduct,
+  productsCount,
 } = require('../controllers/productController');
 
 // routes
 router.post('/product', authCheck, adminCheck, create);
-router.get('/products/:count', listAll); // products/100
+router.get('/products/total', productsCount);
+router.get('/products/:count', listAll);
 router.delete('/product/:slug', authCheck, adminCheck, remove);
 router.get('/product/:slug', read);
 router.put('/product/:slug', authCheck, adminCheck, updateProduct);
