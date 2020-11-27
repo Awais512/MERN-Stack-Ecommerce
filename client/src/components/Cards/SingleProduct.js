97e6ba5a364fa7768cd1da06913c5a612448440b
awais512 <raza.awais25@gmail.com>
2020-11-27 19:23:20 +0500

    Rating Modal

--- client/src/components/Cards/SingleProduct.js
+++ client/src/components/Cards/SingleProduct.js
@@ -7,6 +7,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Laptop from '../../images/laptop.png';
 import ProductListItems from './ProductListItems';
 import StarRatings from 'react-star-ratings';
+import RatingModal from '../Modal/RatingModal';
 
 const { TabPane } = Tabs;
 
@@ -39,14 +40,6 @@ const SingleProduct = ({ product }) => {
 
       <div className='col-md-5'>
         <h1 className='bg-info p-3'>{title}</h1>
-        <StarRatings
-          name={_id}
-          numberOfStars={5}
-          starRatedColor='red'
-          rating={2}
-          changeRating={(newRating, name) => console.log(newRating, name)}
-          isSelectable={true}
-        />
 
         <Card
           actions={[
@@ -57,6 +50,16 @@ const SingleProduct = ({ product }) => {
             <Link to='/'>
               <HeartOutlined className='text-info' /> <br /> Add to Wishlist
             </Link>,
+            <RatingModal>
+              <StarRatings
+                name={_id}
+                numberOfStars={5}
+                starRatedColor='red'
+                rating={2}
+                changeRating={(newRating, name) => console.log(newRating, name)}
+                isSelectable={true}
+              />
+            </RatingModal>,
           ]}
         >
           <ProductListItems product={product} />
