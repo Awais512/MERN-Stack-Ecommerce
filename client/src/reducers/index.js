8b8166896f805a4cc1a5707b28d3c5e472cdc4c3
awais512 <raza.awais25@gmail.com>
2020-12-02 21:12:47 +0500

    Add cart to redux state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { cartReducers } from './cartReducers';
 import { searchReducers } from './searchReducers';
 import { userReducers } from './userReducers';
 
 const rootReducer = combineReducers({
   user: userReducers,
   search: searchReducers,
+  cart: cartReducers,
 });
 
 export default rootReducer;
