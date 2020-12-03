726a5f1f7aff2e1792ab91149f07101cd06bd3d9
awais512 <raza.awais25@gmail.com>
2020-12-03 13:52:26 +0500

    Quantity update in cart

--- client/src/components/Cart/CartTable.js
+++ client/src/components/Cart/CartTable.js
@@ -1,7 +1,9 @@
+import { toSafeInteger } from 'lodash';
 import React from 'react';
 import ModalImage from 'react-modal-image';
 import { useDispatch } from 'react-redux';
 import Laptop from '../../images/laptop.png';
+import { toast } from 'react-toastify';
 
 const CartTable = ({ p }) => {
   const colors = ['Black', 'Brown', 'Silver', 'White', 'Blue'];
@@ -23,6 +25,30 @@ const CartTable = ({ p }) => {
       });
 
       //  console.log('cart udpate color', cart)
+      localStorage.setItem('cart', JSON.stringify(cart));
+      dispatch({
+        type: 'ADD_TO_CART',
+        payload: cart,
+      });
+    }
+  };
+
+  const handleQuantityChange = (e) => {
+    let count = e.target.value < 1 ? 1 : e.target.value;
+    if (count > p.quantity) {
+      toast.error(`Max available quantity: ${p.quantity}`);
+      return;
+    }
+    let cart = [];
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+      }
+      cart.map((product, i) => {
+        if (product._id === p._id) {
+          cart[i].count = count;
+        }
+      });
       localStorage.setItem('cart', JSON.stringify(cart));
       dispatch({
         type: 'ADD_TO_CART',
@@ -66,7 +92,14 @@ const CartTable = ({ p }) => {
               ))}
           </select>
         </td>
-        <td>{p.count}</td>
+        <td className='text-center'>
+          <input
+            type='number'
+            className='form-control'
+            value={p.count}
+            onChange={handleQuantityChange}
+          />
+        </td>
         <td>Shipping Icon</td>
         <td>Delete Icon</td>
       </tr>
