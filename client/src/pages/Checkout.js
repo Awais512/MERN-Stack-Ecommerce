90bca5aed259a713f0d810ea1bfb97c7e08a4a6c
awais512 <raza.awais25@gmail.com>
2020-12-04 19:40:59 +0500

    User address on checkout 2

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { getUserCart, emptyUserCart, saveUserAddress } from '../functions/user';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // ES6
 
 const Checkout = () => {
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState(0);
   const [address, setAddress] = useState('');
+  const [addressSaved, setAddressSaved] = useState(false);
 
   const dispatch = useDispatch();
   const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
     getUserCart(user.token).then((res) => {
+      console.log('user cart res', JSON.stringify(res.data, null, 4));
       setProducts(res.data.products);
       setTotal(res.data.cartTotal);
     });
@@ -36,10 +40,14 @@ const Checkout = () => {
     });
   };
 
-  const saveAddressToDb = async () => {
-    const { data } = await saveUserAddress(user.token, user.address);
-    setAddress(data);
-    console.log(data);
+  const saveAddressToDb = () => {
+    // console.log(address);
+    saveUserAddress(user.token, address).then((res) => {
+      if (res.data.ok) {
+        setAddressSaved(true);
+        toast.success('Address saved');
+      }
+    });
   };
 
   return (
@@ -48,14 +56,10 @@ const Checkout = () => {
         <h4>Delivery Address</h4>
         <br />
         <br />
-        textarea
-        <button
-          className='btn btn-primary mt-2 btn-raised'
-          onClick={saveAddressToDb}
-        >
+        <ReactQuill theme='snow' value={address} onChange={setAddress} />
+        <button className='btn btn-primary mt-2' onClick={saveAddressToDb}>
           Save
         </button>
-        {JSON.stringify(address)}
         <hr />
         <h4>Got Coupon?</h4>
         <br />
@@ -80,14 +84,19 @@ const Checkout = () => {
 
         <div className='row'>
           <div className='col-md-6'>
-            <button className='btn btn-primary btn-raised'>Place Order</button>
+            <button
+              className='btn btn-primary'
+              disabled={!addressSaved || !products.length}
+            >
+              Place Order
+            </button>
           </div>
 
           <div className='col-md-6'>
             <button
               disabled={!products.length}
               onClick={emptyCart}
-              className='btn btn-primary btn-raised'
+              className='btn btn-primary'
             >
               Empty Cart
             </button>
