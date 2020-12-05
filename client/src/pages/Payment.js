68c7986193794c7dd303d556268619524a36c4fa
awais512 <raza.awais25@gmail.com>
2020-12-05 17:04:02 +0500

    Stripe setup Frontend

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const Payment = () => {
-  return <div>Payment Page</div>;
+  return (
+    <div className='container p-5 text-center'>
+      <h4>Complete your Purchase</h4>
+      <Elements stripe={promise}>
+        <div className='col-md-8 offset-md-2'>
+          <p>Stripe Component</p>
+        </div>
+      </Elements>
+    </div>
+  );
 };
 
 export default Payment;
