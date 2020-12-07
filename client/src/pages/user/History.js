5e2b89da2c469aa7c32c6bc7fe1242bd798d45f9
awais512 <raza.awais25@gmail.com>
2020-12-07 13:43:28 +0500

    User Orders Frontend

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserNav from '../../components/nav/UserNav';
+import { getUserOrders } from '../../functions/user';
+import { useSelector } from 'react-redux';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { toast } from 'react-toastify';
 
 const History = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [orders, setOrders] = useState([]);
+
+  const loadUserOrders = async () => {
+    getUserOrders(user.token).then(({ data }) => {
+      console.log(data);
+      setOrders(data);
+    });
+  };
+
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
   return (
     <div className='container-fluid'>
       <div className='row'>
         <div className='col-md-2'>
           <UserNav />
         </div>
-        <div className='col'>User History Page</div>
+        <div className='col'>
+          <h4>User History Page</h4>
+          {JSON.stringify(orders)}
+        </div>
       </div>
     </div>
   );
